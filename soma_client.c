4c170ca144dc0f3d1233521387415a0e820122d5
imMatheusMagalhaes <magalhaes.almeida.matheus@gmail.com>
2021-02-03 12:05:55 -0300

    code

--- soma_client.c
+++ soma_client.c
@@ -6,46 +6,47 @@
 
 #include "soma.h"
 
-
-void
-add_prog_1(char *host, int x, int y)
+void add_prog_1(char *host, int x, int y)
 {
 	CLIENT *clnt;
-	int  *result_1;
-	numbers  add_1_arg;
+	int *result_1;
+	numbers add_1_arg;
 
-#ifndef	DEBUG
-	clnt = clnt_create (host, ADD_PROG, ADD_VERS, "udp");
-	if (clnt == NULL) {
-		clnt_pcreateerror (host);
-		exit (1);
+#ifndef DEBUG
+	clnt = clnt_create(host, ADD_PROG, ADD_VERS, "udp");
+	if (clnt == NULL)
+	{
+		clnt_pcreateerror(host);
+		exit(1);
 	}
-#endif	/* DEBUG */
-	add_1_arg.a=x;
-	add_1_arg.b=y;
+#endif /* DEBUG */
+	add_1_arg.a = x;
+	add_1_arg.b = y;
 
 	result_1 = add_1(&add_1_arg, clnt);
-	if (result_1 == (int *) NULL) {
-		clnt_perror (clnt, "call failed");
-	} else {
-	printf("Resultado: %d\n", *result_1);
+	if (result_1 == (int *)NULL)
+	{
+		clnt_perror(clnt, "call failed");
 	}
-#ifndef	DEBUG
-	clnt_destroy (clnt);
-#endif	 /* DEBUG */
+	else
+	{
+		printf("Resultado: %d\n", *result_1);
+	}
+#ifndef DEBUG
+	clnt_destroy(clnt);
+#endif /* DEBUG */
 }
 
-
-int
-main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	char *host;
 
-	if (argc < 4) {
-		printf ("usage: %s server_host numero1 numero2\n", argv[0]);
-		exit (1);
+	if (argc < 4)
+	{
+		printf("usage: %s server_host numero1 numero2\n", argv[0]);
+		exit(1);
 	}
 	host = argv[1];
-	add_prog_1 (host, atoi(argv[2]), atoi(argv[3]));
-exit (0);
+	add_prog_1(host, atoi(argv[2]), atoi(argv[3]));
+	exit(0);
 }
--- soma_server.c
+++ soma_server.c
@@ -6,15 +6,15 @@
 
 #include "soma.h"
 
-int *
-add_1_svc(numbers *argp, struct svc_req *rqstp)
+int *add_1_svc(numbers *argp, struct svc_req *rqstp)
 {
-	static int  result;
+	static int result;
 
 	/*
 	 * insert server code here
 	 */
 	printf("Funcao soma(%d, %d) foi chamada\n", argp->a, argp->b);
+
 	result = argp->a + argp->b;
 
 	return &result;
